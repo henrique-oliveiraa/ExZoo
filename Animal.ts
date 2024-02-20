1dff9944707d5c50619ea3903f91a406cf1d085c
henrique-oliveiraa <148977691+henrique-oliveiraa@users.noreply.github.com>
2024-02-19 21:37:30 -0300

    Update Animal.ts

--- Animal.ts
+++ Animal.ts
@@ -3,11 +3,16 @@ export class Animal {
 private name:string
 private age:number
 
-constructor (name:string,age:number){
+constructor (name:string,age:number,weight:number){
     this.name = name
     this.age = age
+    this.weight = number
 }
 
+    eat(quantity: number): void {
+  console.log(`The animal has eaten ${quantity} grams of food`);
+
+}  
 emitSound():void{
     console.log("the animal makes a sound");
     
